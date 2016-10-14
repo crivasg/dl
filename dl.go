bb58de35116b0f32b425836c73c8d585b386d901
Cesar Rivas <crivasg@gmail.com>
2016-10-13 20:44:47 -0500

    fromFile and formArgs cli.Commands added

--- dl.go
+++ dl.go
@@ -8,6 +8,52 @@ import (
 
 var app *cli.App
 
+func fromFileCommand() cli.Command {
+	command := cli.Command{
+		Name:      "formFile",
+		ShortName: "ff",
+		Usage:     "Obtain URLs from filename",
+		Action:    fromFile,
+
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "filename",
+				Value: "",
+				Usage: "Filename containing the URLs to download",
+			},
+		},
+	}
+	return command
+}
+
+func fromFile(ctx *cli.Context) {
+	filename := ctx.String("filename")
+	fmt.Printf("fromFile %s\n", filename)
+}
+
+func fromArgsCommand() cli.Command {
+	command := cli.Command{
+		Name:      "formArgs",
+		ShortName: "fa",
+		Usage:     "Obtain URLs from command line arguments",
+		Action:    fromArgs,
+
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "arguments",
+				Value: "",
+				Usage: "URLs to download",
+			},
+		},
+	}
+	return command
+}
+
+func fromArgs(ctx *cli.Context) {
+	arguments := ctx.String("arguments")
+	fmt.Printf("fromArgs %s\n", arguments)
+}
+
 func main() {
 
 	app = cli.NewApp()
